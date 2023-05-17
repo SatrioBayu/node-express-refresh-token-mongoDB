1ada7355ce903bce886de588de0eab21c18d7a6c
SatrioBayu <sbayu875@gmail.com>
2023-05-17 13:52:50 +0700

    feat: add test case for register

--- app/controller/authenticationController.test.js
+++ app/controller/authenticationController.test.js
@@ -2,21 +2,40 @@ const authenticationController = require("./authenticationController");
 const User = require("../models/userModel");
 const mongoose = require("mongoose");
 
+beforeAll(async () => {
+  await mongoose.connect("mongodb://localhost:27017/learn-mongodb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+// Close the connection to the database after the tests have finished
+afterAll(async () => {
+  await User.findOneAndDelete({ username: "jabran" }, { collation: { locale: "en", strength: 2 } });
+  await mongoose.disconnect();
+});
+
 describe("authenticationController", () => {
   describe("registerUser", () => {
-    beforeAll(async () => {
-      await mongoose.connect("mongodb://localhost:27017/your-database", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+    it("should return 500 if there's error in asynchronous function", async () => {
+      const mockReq = {};
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      await authenticationController.handleRegister(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(500);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: "E-003",
+            message: "Something went wrong",
+          },
+        ],
       });
     });
-
-    // Close the connection to the database after the tests have finished
-    afterAll(async () => {
-      await mongoose.disconnect();
-    });
-
-    it("should return 400 if the body is empty", () => {
+    it("should return 400 if the body is empty", async () => {
       const mockReq = {
         body: {},
       };
@@ -25,20 +44,18 @@ describe("authenticationController", () => {
         send: jest.fn().mockReturnThis(),
       };
 
-      authenticationController.handleRegister(mockReq, mockRes, () => {
-        expect(mockRes.status).toHaveBeenCalledWith(400);
-        expect(mockRes.send).toHaveBeenCalledWith({
-          errors: [
-            {
-              code: "E-001",
-              message: "Please provide a username and password",
-            },
-          ],
-        });
-        done();
+      authenticationController.handleRegister(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(400);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: "E-001",
+            message: "Please provide a username and password",
+          },
+        ],
       });
     });
-    it("should return 409 if the username already exists", () => {
+    it("should return 409 if the username already exists", async () => {
       const mockReq = {
         body: {
           username: "bayu",
@@ -51,16 +68,36 @@ describe("authenticationController", () => {
         send: jest.fn().mockReturnThis(),
       };
 
-      authenticationController.handleRegister(mockReq, mockRes, () => {
-        expect(mockRes.status).toHaveBeenCalledWith(409);
-        expect(mockRes.send).toHaveBeenCalledWith({
-          errors: [
-            {
-              code: "E-002",
-              message: "Username already exists",
-            },
-          ],
-        });
+      await authenticationController.handleRegister(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(409);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: "E-002",
+            message: "Username already exists",
+          },
+        ],
+      });
+    });
+    it("should return 200 if everything is OK", async () => {
+      const mockReq = {
+        body: {
+          username: "Jabran",
+          password: "password",
+        },
+      };
+
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+
+      await authenticationController.handleRegister(mockReq, mockRes);
+
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        message: "User registered successfully",
+        user: expect.any(Object),
       });
     });
   });
