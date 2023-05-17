a160a49edafc90581a86f9c14f344c7a434d8138
SatrioBayu <sbayu875@gmail.com>
2023-05-17 14:38:04 +0700

    feat: write handleLogin function to fulfill test case

--- app/controller/authenticationController.js
+++ app/controller/authenticationController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const handleRegister = async (req, res) => {
   try {
@@ -39,7 +40,7 @@ const handleRegister = async (req, res) => {
       user: newUser,
     });
   } catch (error) {
-    return res.status(500).send({
+    res.status(500).send({
       errors: [
         {
           code: "E-003",
@@ -50,4 +51,85 @@ const handleRegister = async (req, res) => {
   }
 };
 
-module.exports = { handleRegister };
+const handleLogin = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).send({
+        errors: [
+          {
+            code: "E-001",
+            message: "Please provide a username and password",
+          },
+        ],
+      });
+    }
+    const user = await User.findOne({ username }).exec();
+    if (!user) {
+      return res.status(401).send({
+        errors: [
+          {
+            code: "E-004",
+            message: "Invalid username or password",
+          },
+        ],
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).send({
+        errors: [
+          {
+            code: "E-004",
+            message: "Invalid username or password",
+          },
+        ],
+      });
+    }
+    const refreshTokenCookie = req.cookies.refreshToken;
+    if (refreshTokenCookie)
+      return res.status(403).send({
+        errors: [
+          {
+            code: "E-005",
+            message: "You are already logged in",
+          },
+        ],
+      });
+
+    const accessToken = generateAccessToken(user);
+    const refreshToken = jwt.sign({ id: user.id }, process.env.REFRESH_TOKEN_SECRET_KEY);
+
+    // PROD
+    // res.cookie("refreshToken", refreshToken, {
+    //   httpOnly: true,
+    //   maxAge: 1000 * 60 * 60 * 24 * 7,
+    // });
+
+    // DEV
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      maxAge: 1000 * 60,
+    });
+
+    res.status(200).send({
+      accessToken,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({
+      errors: [
+        {
+          code: "E-003",
+          message: "Something went wrong",
+        },
+      ],
+    });
+  }
+};
+
+const generateAccessToken = (user) => {
+  return jwt.sign({ id: user.id }, process.env.ACCESS_TOKEN_SECRET_KEY, { expiresIn: "30m" });
+};
+
+module.exports = { handleRegister, handleLogin };
