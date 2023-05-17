7a1e43faeb8cc075de6c28de816676813fcfb927
SatrioBayu <sbayu875@gmail.com>
2023-05-17 15:40:44 +0700

    feat: user the validation created previously and create endpoint

--- app/routes/userRouter.js
+++ app/routes/userRouter.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { authenticationController } = require("../controller");
+const bodyValidation = require("../validation/bodyValidation");
+const validationResult = require("../validation/validationResult");
 
 router.get("/", (req, res) => {
   res.send({
     message: "Router user",
   });
 });
+
+router.post("/register", bodyValidation.authValidate, validationResult.validate, authenticationController.handleRegister);
+router.post("/login", bodyValidation.loginValidate, validationResult.validate, authenticationController.handleLogin);
 
 module.exports = router;
