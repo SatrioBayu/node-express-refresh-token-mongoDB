191c6cadef49379c773b61ee07a0191dd1345bfe
SatrioBayu <sbayu875@gmail.com>
2023-05-23 08:54:43 +0700

    feat: add unit test for update password

--- app/controller/userController.test.js
+++ app/controller/userController.test.js
@@ -121,4 +121,94 @@ describe("User Controller", () => {
       });
     });
   });
+
+  describe("Update Password", () => {
+    it("should return 500 if there's error in try catch code", async () => {
+      const mockReq = {};
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+      await userController.handleUpdatePassword(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(500);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: "E-007",
+            message: expect.any(String),
+          },
+        ],
+      });
+    });
+    it("should return 403 if the user id in token didn't match with user id in query", async () => {
+      const mockReq = {
+        body: {
+          oldPassword: "123456",
+          newPassword: "1234567",
+        },
+        user: {
+          id: "64645ad046304d62536a3c15",
+        },
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+      await userController.handleUpdatePassword(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(403);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: "E-012",
+            message: "User from this token not found",
+          },
+        ],
+      });
+    });
+    it("should return 409 if the old password is wrong", async () => {
+      const mockReq = {
+        body: {
+          oldPassword: "123456",
+          newPassword: "1234567",
+        },
+        user: {
+          id: "64673884dd70d7c7ca4dc599",
+        },
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+      await userController.handleUpdatePassword(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(409);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        errors: [
+          {
+            code: "E-013",
+            message: "Old password is incorrect",
+          },
+        ],
+      });
+    });
+    it("should return 200 if everything is OK", async () => {
+      const mockReq = {
+        body: {
+          oldPassword: "12345678",
+          newPassword: "12345678",
+        },
+        user: {
+          id: "64673884dd70d7c7ca4dc599",
+        },
+      };
+      const mockRes = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn().mockReturnThis(),
+      };
+      await userController.handleUpdatePassword(mockReq, mockRes);
+      expect(mockRes.status).toHaveBeenCalledWith(200);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        message: "Password successfully updated",
+      });
+    });
+  });
 });
